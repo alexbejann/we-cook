8ee0fd2b1b140398c2ecc27f9c76679affa3f79a
alexbejann <alex_bejan11@yahoo.com>
2023-06-04 22:33:11 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.we_cook_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:we_cook_app/app/app.dart';
+import 'package:we_cook_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
